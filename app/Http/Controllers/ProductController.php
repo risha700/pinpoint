6d8c36ba4dee345020ade62be9d1a0ad424331c3
Ahmed Shehab <ahbox@outlook.com>
2018-10-02 19:04:59 -0500

    debugging /product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -29,7 +29,7 @@ class ProductController extends Controller
 
 
         if(request()->wantsJson()){
-            
+
             $products = Product::with('photos')->get();
             return $products;
         }
